c0ecdfe04450e3ef6e0ea13124855b9cab813a13
Hardik Patel <hardik.patel@td.com>
2015-05-22 15:02:08 -0400

    Parameterize server.js

--- node-server/server/boot/create-data-model.js
+++ node-server/server/boot/create-data-model.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-  /*
+  /**
   app.dataSources.hnpDs.automigrate('user', function(err) {
     if (err) throw err;
     console.log('Models created:');
@@ -22,50 +22,6 @@ module.exports = function(app) {
     if (err) throw err;
     console.log('Models created:');
   });
-  
-  
-  function modifyResponse(ctx, model, next) {
-    ctx.res.set('Access-Control-Expose-Headers', 'Location');
-  }
-
-  app.models.user.afterRemote('**', modifyResponse);
-  app.models.driver.afterRemote('**', modifyResponse);
-  app.models.customer.afterRemote('**', modifyResponse);
-  app.models.car.afterRemote('**', modifyResponse);
-  app.models.event.afterRemote('**', modifyResponse);
- 
-  
-  var User = app.models.user;
-  var Role = app.models.Role;
-  var RoleMapping = app.models.RoleMapping;
-  
-  User.create([
-    {username: 'John', email: 'john@doe.com', password: 'opensesame', firstName: 'John', lastName: 'Smith', mobile: 9898298982 },
-    {username: 'Jane', email: 'jane@doe.com', password: 'opensesame', firstName: 'Jane', lastName: 'Smith', mobile: 9898298982 },
-    {username: 'Bob', email: 'bob@projects.com', password: 'opensesame', firstName: 'Bob', lastName: 'Smith', mobile: 9898298982 }
-  ], function(err, users) {
-    if (err) throw err;
-
-    console.log('Created users:', users);
-
-    //create the admin role
-    Role.create({
-      name: 'admin'
-    }, function(err, role) {
-      if (err) throw err;
-
-      console.log('Created role:', role);
-
-      //make bob an admin
-      role.principals.create({
-        principalType: RoleMapping.USER,
-        principalId: users[2].id
-      }, function(err, principal) {
-        if (err) throw err;
-
-        console.log('Created principal:', principal);
-      });
-    });
-  });         */
+  **/
 
 };
